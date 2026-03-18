70aa4100d7433a8b20fb383c631a7bbecb5c55ef
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 06:10:04 -0500

    Attach public IP to network interface

--- main.tf
+++ main.tf
@@ -80,10 +80,11 @@ resource "azurerm_network_interface" "nic" {
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+  name                          = "internal"
+  subnet_id                     = azurerm_subnet.subnet.id
+  private_ip_address_allocation = "Dynamic"
+  public_ip_address_id          = azurerm_public_ip.public_ip.id
+}
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
