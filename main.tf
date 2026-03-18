8cc6d6e1d4f99265a0a56fc5f446f67463c33736
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 06:08:22 -0500

    Add public IP for virtual machine

--- main.tf
+++ main.tf
@@ -21,6 +21,15 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+# ---------------------------
+# Public IP
+# ---------------------------
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-vm-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
 
 # ---------------------------
 # Virtual Network + Subnet
