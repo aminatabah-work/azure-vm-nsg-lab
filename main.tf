80b19336a696beec90ef7e061856fbbad7ba8d1a
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 05:48:59 -0500

    Add Azure network security group

--- main.tf
+++ main.tf
@@ -31,3 +31,9 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-vm-lab"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
