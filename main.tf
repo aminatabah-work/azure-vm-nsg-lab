72a4d43b9d2e4865a873acd3777a3afc27bb5a81
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 05:47:46 -0500

    Add Azure resource group, virtual network, and subnet

--- main.tf
+++ main.tf
@@ -12,3 +12,22 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-vm-nsg-lab"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-vm-nsg-lab"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-app"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
