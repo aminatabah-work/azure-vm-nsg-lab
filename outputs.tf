f7e855f9e84499db68f4d2cbefaebe63415d4420
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 06:24:18 -0500

    Update outputs for VM, NSG, network interface, and public IP

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,11 @@ output "network_security_group_name" {
 output "virtual_network_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+output "network_interface_name" {
+  value = azurerm_network_interface.nic.name
+}
+
+output "public_ip_name" {
+  value = azurerm_public_ip.public_ip.name
+}
