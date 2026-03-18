4161c0f715bb101a96acb90cc1fd4ae151d1b306
aminatabah-work <aminata.bah.work@gmail.com>
2026-03-18 05:55:29 -0500

    Update outputs for VM, NSG, and virtual network

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "vm_name" {
   value = azurerm_linux_virtual_machine.vm.name
 }
+
+output "network_security_group_name" {
+  value = azurerm_network_security_group.nsg.name
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.vnet.name
+}
